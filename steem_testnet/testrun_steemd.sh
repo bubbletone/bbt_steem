e5234e0b55519e83aa70eeb463c637528e13c951
Local User <spriludskiy@srvap052.holding.allo>
2017-12-01 13:30:34 +0300

    simplification of test node deployment - iter

--- steem_testnet/testrun_steemd.sh
+++ steem_testnet/testrun_steemd.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# catalog with blockchain, p2p tmp files. Must reside on SSD!
+STEEMD_WORK_DIR=/var/tmp/steemd
+mkdir -p $STEEMD_WORK_DIR
+
 mkdir -p $HOME/steem/build
 cd $HOME/steem/build
 
-cp $HOME/bbt_steem/steem_testnet/config.ini /var/lib/steemd/config.ini && programs/steemd/steemd -d /var/lib/steemd --enable-plugin bbtone $1
+# exit if we haven't write access
+if [ -w $STEEMD_WORK_DIR ]; then echo "Dir: $STEEMD_WORK_DIR is ok"; else echo ""No write acces to dir: $STEEMD_WORK_DIR"; exit 3;"; fi
+
+# copy config.ini from repo
+echo "Replacing  $HOME/bbt_steem/steem_testnet/config.ini -> $STEEMD_WORK_DIR/config.ini. Disable if needed"
+cp $HOME/bbt_steem/steem_testnet/config.ini $STEEMD_WORK_DIR/config.ini && programs/steemd/steemd -d $STEEMD_WORK_DIR --enable-plugin bbtone $1
