2c30920b63d96defec8a9be960940bec822042bb
Local User <spriludskiy@srvap052.holding.allo>
2017-12-01 13:10:20 +0300

    small fix for simplification of test node deployment

--- steem_testnet/testrun_steemd.sh
+++ steem_testnet/testrun_steemd.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+mkdir -p $HOME/steem/build
+cd $HOME/steem/build
+
 cp $HOME/bbt_steem/steem_testnet/config.ini /var/lib/steemd/config.ini && programs/steemd/steemd -d /var/lib/steemd --enable-plugin bbtone $1
